ace51a9350b0566417ce22d1a5dc17e921ebb0be
Vinicius-moura-code <viniciusrebelo487@gmail.com>
2021-01-31 23:18:22 -0300

    First exemple

---
+++ FirstApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int numeroDeVezes = 5;
+
+      for (int i = 0; i < numeroDeVezes; i++)
+      {
+        Console.WriteLine($"Bem vindo a .NET BAKA {i}");
+      }
+    }
+  }
+}
