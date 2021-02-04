bf0daa93c60fe4605de5925a58311799a105b7ac
Vinicius-moura-code <viniciusrebelo487@gmail.com>
2021-02-03 22:45:20 -0300

    Update apps

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc; Integrated Security=True");
+
+        }
+    }
+
+
+}
