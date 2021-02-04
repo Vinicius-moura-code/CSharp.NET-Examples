c57f8c4ccdde5f9028369a2566bb5daf233bcf55
Vinicius-moura-code <viniciusrebelo487@gmail.com>
2021-02-03 23:47:37 -0300

    Start Tests

--- CursoMVC/CursoTest/CategoriaControllerTest.cs
+++ CursoMVC/CursoTest/CategoriaControllerTest.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CursoAPI.Controllers;
+using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
 
 namespace CursoTest
 {
-    class CategoriaControllerTest
+    public class CategoriaControllerTest
     {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriaControllerTest()
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+        }
+
+        [Fact]
+
+        public async Task Get_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+
+            await service.GetCategoria(1);
+
+            _mockSet.Verify(m => m.FindAsync(1),
+                Times.Once());
+
+        }
     }
 }
+
+
+
