6825dd87daf2d4bea16d835f129e902240eba40f
Vinicius-moura-code <viniciusrebelo487@gmail.com>
2021-02-08 00:59:42 -0300

    Desafios

---
+++ Algoritmos/Algoritmos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algoritmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            string[] data = input.Split(' ');
+
+            int h = int.Parse(data[0]);
+            int v = int.Parse(data[1]);
+            double qtdCombustivelGasto = h * v / 12.0;
+
+
+            Console.WriteLine(Math.Round(qtdCombustivelGasto, 3));
+        }
+    }
+}
