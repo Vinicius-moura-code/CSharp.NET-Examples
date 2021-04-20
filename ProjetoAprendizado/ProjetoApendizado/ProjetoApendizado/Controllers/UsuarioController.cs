72bff1bdcdb76c3b99f61cc9fa4845fbd540d13b
Vinicius-moura-code <viniciusrebelo487@gmail.com>
2021-04-20 13:32:32 -0300

    Insert meta

---
+++ ProjetoAprendizado/ProjetoApendizado/ProjetoApendizado/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoApendizado.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoAprendizado/ProjetoApendizado/ProjetoApendizado/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoApendizado.Models
+{
+    public class Usuario
+    {
+        //Nome, idade, data de nascimento, sexo, cpf.
+
+        public string Nome { get; set;}
+
+        public int Idade { get; set; }
+
+        public DateTime DatadeNascimeto { get; set; }
+
+        public string Sexo { get; set; }
+
+        public int Cpf { get; set;  }
+
+    }
+} 
